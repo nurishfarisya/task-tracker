7174ee55d0918908e9772350cdf5a552de65cc0e
nurishfarisya <nurish.farisya@gmail.com>
2025-07-18 14:30:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+  delBtn.onclick = () => li.remove();
+
+  li.appendChild(delBtn);
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
